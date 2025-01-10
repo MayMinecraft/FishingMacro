195f876456ad879a6f4733a06718f3269326076d
MayMinecraft <may_heruko@proton.me>
2025-01-10 20:22:37 +0100

    Update macro.js

--- features/macro.js
+++ features/macro.js
@@ -1,14 +1,17 @@
 /// <reference types="../../CTAutocomplete" />
 
 const EntityArmorStand = Java.type("net.minecraft.entity.item.EntityArmorStand")
-const EntityFishHook = Java.type("net.minecraft.entity.projectile.EntityFishHook") // a bobber
 const C08PacketPlayerBlockPlacement = Java.type("net.minecraft.network.play.client.C08PacketPlayerBlockPlacement")
 const C0APacketAnimation = Java.type("net.minecraft.network.play.client.C0APacketAnimation")
+const EntityFishHook = Java.type("net.minecraft.entity.projectile.EntityFishHook") // a bobber
 
 var PlayerBobberX = undefined
 var PlayerBobberZ = undefined
-var fishing = false
 var fishingTime = undefined
+var fishing = false
+
+let playerYawDist = 0
+let playerPitchDist = 0
 
 export const DetectFish = register("step", () => {
     World.getAllEntitiesOfType(EntityArmorStand).forEach((fish) => {
@@ -19,7 +22,7 @@ export const DetectFish = register("step", () => {
             ReelIn()
             setTimeout(() => {
                 fishing = false
-            }, 3000)
+            }, 500)
         }
     })
 }).setFps(20).unregister()
@@ -27,7 +30,7 @@ export const DetectFish = register("step", () => {
 function IsAllowed() {
     const currentGui = Client.getMinecraft().field_71462_r
     if (!currentGui || currentGui?.getClass()?.getSimpleName?.().includes('GuiChat')) return true
-    
+
     return false
 }
 
@@ -36,7 +39,14 @@ function ReelIn() {
     Reel()
     setTimeout(() => {
         Reel()
+        rotate()
+
     }, 200)
+}
+
+export function reset(){
+    playerYawDist = 0
+    playerPitchDist = 0
 }
 
 function Reel() {
@@ -45,4 +55,18 @@ function Reel() {
 
     Client.sendPacket(new C08PacketPlayerBlockPlacement(item.itemStack))
     Client.sendPacket(new C0APacketAnimation())
+}
+
+function rotate(){
+    playerYawDist < 0 ? playerYawDist = playerYawDist + getRandomInt() : playerYawDist = playerYawDist - getRandomInt()
+    playerPitchDist < 0 ? playerPitchDist = playerPitchDist + getRandomInt() : playerPitchDist = playerPitchDist - getRandomInt()
+
+    const player = Player.getPlayer()
+
+    player.field_70177_z = (player.field_70177_z + playerYawDist)
+    player.field_70125_A = (player.field_70125_A + playerPitchDist)
+}
+
+function getRandomInt() {
+    return Math.floor(Math.random() * 30) / 10
 }
